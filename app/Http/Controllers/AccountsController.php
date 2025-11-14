10a39e61a3593048494197bfcbb457efcf9d8c85
Marvin Quezon <quezon.marvin@gmail.com>
2025-11-14 19:23:29 +0800

    Update AccountsController.php

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -5,14 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Auth;
 
 class AccountsController extends Controller
 {
     public function index(Request $request)
     {
         $accounts = User::select('id', 'name', 'email')
-            ->where('id', 'like', '%' . $request->search . '%')
-            ->orWhere('email', 'like', '%' . $request->search . '%')
+            ->where(function ($query) use ($request) {
+                $query->where('id', 'like', '%' . $request->search . '%')
+                    ->orWhere('name', 'like', '%' . $request->search . '%')
+                    ->orWhere('email', 'like', '%' . $request->search . '%');
+            })
+            ->where('id', '!=', Auth::id())
             ->paginate(50);
 
         return UserResource::collection($accounts);

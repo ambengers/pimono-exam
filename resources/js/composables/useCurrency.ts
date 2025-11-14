e29cf80edf373d841fa916be27cf3fdd31d74cd4
Marvin Quezon <quezon.marvin@gmail.com>
2025-11-14 20:41:40 +0800

    Add currency formatting composable

---
+++ resources/js/composables/useCurrency.ts
@@ -0,0 +1,15 @@
+export function useCurrency() {
+    function formatCurrency(amount: number): string {
+        return new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        }).format(amount);
+    }
+
+    return {
+        formatCurrency,
+    };
+}
+

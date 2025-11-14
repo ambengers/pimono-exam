3038509e7948ed7385726086def139230996c505
Marvin Quezon <quezon.marvin@gmail.com>
2025-11-14 23:55:23 +0800

    Update routes.ts

--- resources/js/routes.ts
+++ resources/js/routes.ts
@@ -1,7 +1,7 @@
 const routes = [
     {
         path: '/',
-        redirect: '/login', // Will be overridden by navigation guard based on auth state
+        redirect: '/login',
     },
     {
         path: '/login',

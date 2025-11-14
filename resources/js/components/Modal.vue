ba39a8ed22b6af4221b3f5081be6bef7b79e96f5
Marvin Quezon <quezon.marvin@gmail.com>
2025-11-14 20:44:43 +0800

    Update Modal.vue

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -66,7 +66,7 @@ onUnmounted(() => {
                             </h3>
                             <button
                                 @click="handleClose"
-                                class="text-gray-400 hover:text-gray-500 focus:outline-none transition-colors"
+                                class="text-gray-400 hover:text-gray-500 focus:outline-none hover:cursor-pointer transition-colors"
                             >
                                 <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />

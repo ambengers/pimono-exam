f983baf545b67665881ab8e906f8b37cc5918fb3
Marvin Quezon <quezon.marvin@gmail.com>
2025-11-14 20:47:29 +0800

    Add transaction icons

---
+++ resources/js/components/icons/ReceiveTransactionIcon.vue
@@ -0,0 +1,15 @@
+<template>
+    <svg
+        class="h-5 w-5 text-green-600"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+    >
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v14m9-9l-9 9-9-9" />
+    </svg>
+</template>
+
+<script setup lang="ts">
+// Receive transaction icon component
+</script>
+
